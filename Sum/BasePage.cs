3c9a57a7e2e42d812271aa84633d129200b5792a
VitaSid <46902014+VitaSid@users.noreply.github.com>
2021-08-30 10:02:05 +0300

    Sum work

---
+++ Sum/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Sum.Page
+{
+    public class BasePage
+    {
+        protected IWebDriver Driver;
+        public BasePage(IWebDriver webdriver)
+        {
+            Driver = webdriver;
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.Quit();
+        }
+               
+    }
+}
---
+++ Sum/Page/SumPage.cs
@@ -0,0 +1,45 @@
+﻿using Sum.Test;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using OpenQA.Selenium.Chrome;
+using Sum.Page;
+
+namespace Sum.page
+{
+    class SumPage : BasePage
+    {
+        public SumPage(IWebDriver webdriver) : base(webdriver) { }
+        IWebElement firstInputField => Driver.FindElement(By.Id("sum1"));
+        IWebElement secondInputField => Driver.FindElement(By.Id("sum2"));
+        IWebElement resultFromPage => Driver.FindElement(By.Id("displayvalue"));
+
+        public void InsertFirstInput(string firstInput)
+        {
+            firstInputField.Clear();
+            firstInputField.SendKeys(firstInput);
+        }
+
+        
+        public void InsertSecondtInput(string secondInput)
+        {
+            secondInputField.Clear();
+            secondInputField.SendKeys(secondInput);
+        }
+
+        public void ClickGetTotalButton()
+        {
+            Driver.FindElement(By.CssSelector("#gettotal > button")).Click();
+        }
+
+        public void VerifyResult (string result)
+        {
+            Assert.AreEqual(result, resultFromPage.Text, $"Actual result differs from expected {result}");
+        }
+
+       
+    }
+
+    
+}
